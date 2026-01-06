3cfe9548bbc009ddebe1019a3d51b1d07ce1a046
Charles Francoise <charles.francoise@circleci.com>
2026-01-06 10:51:44 +0100

    use my eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import pluginJs from '@eslint/js';
-import prettierConfig from 'eslint-config-prettier';
-import importPlugin from 'eslint-plugin-import';
+import baseConfig from 'eslint-config-loderunner/base';
+import formattingConfig from 'eslint-config-loderunner/formatting';
+import importConfig from 'eslint-config-loderunner/import';
+import typescriptConfig from 'eslint-config-loderunner/typescript';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -13,99 +13,8 @@ export default [
   {
     languageOptions: { globals: { ...globals.browser } },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: {
-          allowDefaultProject: ['*.{config,setup}.{js,mjs,cjs,ts}'],
-        },
-      },
-    },
-  },
-  {
-    rules: {
-      eqeqeq: 'error',
-      'no-duplicate-imports': 'error',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          args: 'all',
-          argsIgnorePattern: '^_',
-          caughtErrors: 'all',
-          caughtErrorsIgnorePattern: '^_',
-          destructuredArrayIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          ignoreRestSiblings: true,
-        },
-      ],
-      '@typescript-eslint/switch-exhaustiveness-check': [
-        'warn',
-        { considerDefaultExhaustiveForUnions: true },
-      ],
-      '@typescript-eslint/strict-boolean-expressions': [
-        'error',
-        {
-          allowAny: false,
-          allowNullableBoolean: false,
-          allowNullableEnum: false,
-          allowNullableNumber: false,
-          allowNullableObject: false,
-          allowNullableString: false,
-          allowNumber: false,
-          allowString: false,
-        },
-      ],
-      '@typescript-eslint/prefer-nullish-coalescing': 'error',
-      '@typescript-eslint/no-unnecessary-condition': 'warn',
-      '@typescript-eslint/no-misused-promises': [
-        'error',
-        { checksVoidReturn: false },
-      ],
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        {
-          prefer: 'type-imports',
-          fixStyle: 'inline-type-imports',
-        },
-      ],
-    },
-  },
-  {
-    plugins: { import: importPlugin },
-    settings: {
-      'import/resolver': {
-        typescript: true,
-        node: true,
-      },
-    },
-    rules: {
-      'import/order': [
-        'warn',
-        {
-          'newlines-between': 'always',
-          named: true,
-          alphabetize: { order: 'asc' },
-        },
-      ],
-    },
-  },
-  // {
-  //   files: ['**/*.test.ts'],
-  //   ...vitest.configs.recommended,
-  //   rules: {
-  //     ...vitest.configs.recommended.rules,
-  //     '@typescript-eslint/no-explicit-any': 'off',
-  //     '@typescript-eslint/no-unsafe-assignment': 'off',
-  //     '@typescript-eslint/no-unsafe-member-access': 'off',
-  //   },
-  // },
-  prettierConfig,
-  // Re-enables curly braces for all statements after Prettier config
-  {
-    rules: {
-      curly: ['error', 'all'],
-    },
-  },
+  ...baseConfig,
+  ...typescriptConfig,
+  ...importConfig,
+  ...formattingConfig,
 ];
